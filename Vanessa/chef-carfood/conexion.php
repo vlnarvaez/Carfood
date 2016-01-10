54b08b3335ee53b2c7d7f7e0160da5374d32a9c7
Vanessa Narváez <vlnarvaez@utpl.edu.ec>
2016-01-09 23:58:33 -0500

    Revert "Parte final chef"
    This reverts commit 4816493e870b7cbe56db414d7dc040e04ee4dbe7.

--- Vanessa/chef-carfood/conexion.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-$cone=mysql_connect("localhost","root","") or die ('No hay conexion a la base de datos');
-$db=mysql_select_db('db_carfood',$cone) or die ('no existe la base de datos');
-?>
